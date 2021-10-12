56046cfaf67a4febe4e89136446191e3eb027d46
Nevena Stankovic <nevena.stankovic.89@gmail.com>
2021-10-12 20:08:45 +0200

    Add LoginPage class

---
+++ src/pages/LoginPage.java
@@ -0,0 +1,46 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage extends BasicPage {
+
+	public LoginPage(WebDriver driver, WebDriverWait waiter, JavascriptExecutor js) {
+		super(driver, waiter, js);
+
+	}
+
+	public WebElement getUsername() {
+		return this.driver.findElement(By.name("username"));
+
+	}
+
+	public WebElement getPassword() {
+		return this.driver.findElement(By.name("password"));
+
+	}
+
+	public WebElement getRememberMe() {
+		return this.driver.findElement(By.name("remember_me"));
+
+	}
+
+	public WebElement getSubmitBtn() {
+		return this.driver.findElement(By.name("btn_submit"));
+
+	}
+
+	public void login(String username, String password) {
+
+		this.getUsername().clear();
+		this.getUsername().sendKeys(username);
+		this.getPassword().clear();
+		this.getPassword().sendKeys(password);
+		this.getSubmitBtn().click();
+
+	}
+
+}
