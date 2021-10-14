cf8dbe1970f22a3360decce196d0fce8dcfa3af8
Nevena Stankovic <nevena.stankovic.89@gmail.com>
2021-10-14 17:33:48 +0200

    Add AuthPage class

---
+++ src/pages/AuthPage.java
@@ -0,0 +1,40 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AuthPage extends BasicPage {
+
+	public AuthPage(WebDriver driver, WebDriverWait waiter, JavascriptExecutor js) {
+		super(driver, waiter, js);
+
+	}
+
+	public WebElement getNameField() {
+
+		return this.driver.findElement(By.xpath("//*[contains(@class,'after-arrow')]"));
+
+	}
+	
+	public WebElement getMyAccount() {
+
+		return this.driver.findElement(By.xpath("//*[contains(text(),'My Account')]"));
+
+	}
+	
+	public WebElement getLogout() {
+
+		return this.driver.findElement(By.xpath("//*[contains(text(),'Logout')]"));
+
+	}
+	
+	public void userLogout () {
+		
+		this.getNameField().click();
+		this.getLogout().click();
+	}
+
+}
